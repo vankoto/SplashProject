3bf74117a1a3e22b7855d9e2c5333309b12e45f2
Ivan Ivanov <ivanvelichkov357@gmail.com>
2018-05-22 18:31:48 +0200

    simple jquery for pop-up car images

---
+++ scripts/custom-js.js
@@ -0,0 +1,25 @@
+//Some simple code for services pop up images.
+function open1() {
+    $('.show-overlay:eq(0)').fadeIn();
+}
+function close1() {
+    $('.show-overlay:eq(0)').fadeOut();
+}
+function open2() {
+    $('.show-overlay:eq(1)').fadeIn();
+}
+function close2() {
+    $('.show-overlay:eq(1)').fadeOut();
+}
+function open3() {
+    $('.show-overlay:eq(2)').fadeIn();
+}
+function close3() {
+    $('.show-overlay:eq(2)').fadeOut();
+}
+function open4() {
+    $('.show-overlay:eq(3)').fadeIn();
+}
+function close4() {
+    $('.show-overlay:eq(3)').fadeOut();
+}
