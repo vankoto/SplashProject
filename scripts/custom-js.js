73abeb8ce841d2ffd094a3b8131d155620add988
Ivan Ivanov <ivanvelichkov357@gmail.com>
2018-05-30 23:23:52 +0200

    Added facebook link and share button

--- scripts/custom-js.js
+++ scripts/custom-js.js
@@ -57,3 +57,12 @@ function splashMap() {
     map: map
   });
 }
+
+///--- Code for facebook like button. ---///
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/dk_DK/sdk.js#xfbml=1&version=v3.0';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
