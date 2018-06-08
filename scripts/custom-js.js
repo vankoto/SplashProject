87be5d103059c6aabd490155e2eabacb677b1f06
Ivan Ivanov <ivanvelichkov357@gmail.com>
2018-06-08 15:10:25 +0200

    Little bracket change

--- scripts/custom-js.js
+++ scripts/custom-js.js
@@ -3,10 +3,10 @@ $("#slideshow").on("touchstart", function(event){
         var xClick = event.originalEvent.touches[0].pageX;
     $(this).one("touchmove", function(event){
         var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
+        if(Math.floor(xClick - xMove) > 5){
             $(this).carousel('next');
         }
-        else if( Math.floor(xClick - xMove) < -5 ){
+        else if(Math.floor(xClick - xMove) < -5){
             $(this).carousel('prev');
         }
     });
